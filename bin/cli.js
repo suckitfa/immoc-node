095d273bc7f15dc160d960707284773d2583ccaa
Guangyuan <254202042@qq.com>
2023-05-26 09:51:51 +0800

    feat:使用commander写一些简单的命令create，体验默认的命令help

--- bin/cli.js
+++ bin/cli.js
@@ -4,5 +4,14 @@ const { program } = require('commander')
 // 从process.argv中获取用户输入的参数
 // console.log(process.argv)
 program.version('0.0.1')
+// 创建create命令
+program
+    .command('create <project> [other...]')
+    .alias('crt')
+    .description('创建一个新的项目')
+    .action((projectName,otherArgs) => {
+        console.log('create', projectName, otherArgs)
+    })
+program.option('-f --framework <framework>', '设置框架')
 // 让我们的命令行工具支持两个参数
 program.parse(process.argv)
