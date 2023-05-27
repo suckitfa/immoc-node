266e50dade4dc93b4796b71090c0973cb986a64e
Guangyuan <254202042@qq.com>
2023-05-28 01:33:07 +0800

    faet:集成downloadFn模块

--- config.js
+++ config.js
@@ -1,3 +1,6 @@
 module.exports = {
-    framework:['express','koa','egg']
+    framework:['express','koa','egg'],
+    templateRepo: {
+        egg:'direct:git@github.com:suckitfa/template-node-egg.git'
+    }
 }
